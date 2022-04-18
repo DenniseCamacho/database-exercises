f5d9991702369634870a8e65086d35a1c7c1bc85
New Author Name <dennise.e.camacho@gmail.com>
2022-04-18 16:46:52 -0500

    first table 1/2 complete over 30m sales

--- albums_migration.sql
+++ albums_migration.sql
@@ -11,20 +11,20 @@ CREATE TABLE IF NOT EXISTS albums (
     PRIMARY KEY (id)
 );
 
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Travis Dupri', 'Album01', 2020, 1000000, 'hip hop, rnb'),
-       ('TG AGAPE', 'Album02', 2021, 1000000, 'hip hop'),
-       ('HAVI', 'Album03', 2020, 1000000, 'hip hop, rnb'),
-       ('NOBIGDYL', 'Album04', 2020, 10000000, 'hip hop, rnb, rap'),
-       ('BRIGHT', 'Album05', 2020, 1000000, 'dance, electronica'),
-       ('Kutless', 'Album06', 2020, 1000000, 'alternative rock'),
-       ('Wolves at the Gate', 'Album07', 2020, 1000000, 'heavy metal'),
-       ('Silent Planet', 'Album08', 2020, 1000000, 'heavy metal'),
-       ('Red', 'Album09', 2020, 1000000, 'Rock'),
-       ('Skillet', 'Album10', 2020, 1000000, 'Rock'),
-       ('Skillet', 'Album10', 1992, 1000000, 'Rock');
-
-DELETE FROM albums WHERE release_date < 2000; #deletes all under 2000
+# INSERT INTO albums (artist, name, release_date, sales, genre)
+# VALUES ('Travis Dupri', 'Album01', 2020, 1000000, 'hip hop, rnb'),
+#        ('TG AGAPE', 'Album02', 2021, 1000000, 'hip hop'),
+#        ('HAVI', 'Album03', 2020, 1000000, 'hip hop, rnb'),
+#        ('NOBIGDYL', 'Album04', 2020, 10000000, 'hip hop, rnb, rap'),
+#        ('BRIGHT', 'Album05', 2020, 1000000, 'dance, electronica'),
+#        ('Kutless', 'Album06', 2020, 1000000, 'alternative rock'),
+#        ('Wolves at the Gate', 'Album07', 2020, 1000000, 'heavy metal'),
+#        ('Silent Planet', 'Album08', 2020, 1000000, 'heavy metal'),
+#        ('Red', 'Album09', 2020, 1000000, 'Rock'),
+#        ('Skillet', 'Album10', 2020, 1000000, 'Rock'),
+#        ('Skillet', 'Album10', 1992, 1000000, 'Rock');
+#
+# DELETE FROM albums WHERE release_date < 2000; #deletes all under 2000
 
 SELECT 'albums' as prefix , a.* FROM albums a WHERE release_date > 2021;
 
--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,1 +1,30 @@
 USE codeup_test_db;
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES
+    ('Michael Jackson', 'Thriller', 1982,  49200000,
+     'pop, post-disco, funk, rock'),
+    ('AC/DC', 'Back in Black', 1980, 29500000,
+     'hard rock'),
+    ('Whitney Houston', 'The Bodyguard', 1992, 32400000,
+     'R&B, soul, pop, soundtrack'),
+    ('Meat Loaf', 'Bat Out of Hell', 1977, 21700000,
+     'hard rock, glam rock, progressive rock'),
+    ('Eagles', 'Their Greatest Hits', 1976, 41200000,
+     'country rock, soft rock, folk rock'),
+    ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24400000,
+     'progressive rock'),
+    ('Eagles', 'Hotel California', 1976, 31500000,
+     'soft rock'),
+    ('Bee Gees', 'Saturday Night Fever', 1977, 21600000,
+     'disco'),
+    ('Fleetwood Mac', 'Rumors', 1977, 27900000,
+     'soft rock'),
+    ('Shania Twain', 'Come On Over', 1997, 29600000,
+     'country, pop');
+
+
+
+#
+# Use INSERT to add records for all the albums from this list on Wikipedia that claim over 30 million sales #(the first two tables). For sales data, use the 'sales certification' column of the tables, not 'claimed #sales'. For artists listed with 'Various Artists', just use the primary artist's name.
+
