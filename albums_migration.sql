bb9268cc234d19b5353466e33acf6414e6a8c59d
New Author Name <dennise.e.camacho@gmail.com>
2022-04-18 14:30:48 -0500

    albums_migration.sql changing to keep consistency

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100) NOT NULL,
     name VARCHAR(50) NOT NULL,
-    release_date INT unsigned,
+    release_date INT UNSIGNED,
     sales FLOAT UNSIGNED NOT NULL,
     genre VARCHAR(150),
     PRIMARY KEY (id)
