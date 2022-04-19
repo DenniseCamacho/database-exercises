53b23016d1f9370a1e8c8e311e7098e957a54848
New Author Name <dennise.e.camacho@gmail.com>
2022-04-19 14:59:55 -0500

    Find all employees with first names Irena, Vidya, or Maya

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,6 @@
 USE employees;
-
-
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+# Find all employees with first names 'Irena', 'Vidya',
+# or 'Maya' — 709 rows (Hint: Use IN).
