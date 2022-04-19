d24f4bee032ff578ae506b603d4fd215a032d7c2
New Author Name <dennise.e.camacho@gmail.com>
2022-04-19 10:54:45 -0500

    appropriate captions before each SELECT

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,9 +1,7 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE release_date > 1991;
-SELECT * FROM albums WHERE genre = 'disco';
-SELECT * FROM albums WHERE artist = 'One Singer';
-# Write SELECT statements for:
-# Albums released after 1991
-# Albums with the genre 'disco'
-#     Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+SELECT 'will be deleted' AS prefix , a.* FROM albums a WHERE artist = 'One Singer';
+SELECT 'will be deleted' AS prefix , a.* FROM albums a WHERE release_date > 1991;
+SELECT 'will be deleted' AS prefix , a.* FROM albums a WHERE genre = 'disco';
+
+#Make sure to put appropriate captions before each SELECT.
