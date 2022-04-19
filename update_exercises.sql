44f4596dabe24ab5a837c052828b561b828a8958
New Author Name <dennise.e.camacho@gmail.com>
2022-04-19 10:20:37 -0500

    all the albums 10 times more popular (sales * 10)

--- update_exercises.sql
+++ update_exercises.sql
@@ -2,3 +2,8 @@
 SELECT 'All albums in your table.' as prefix , albums.name FROM albums WHERE albums.id >= 0;
 SELECT 'All albums released before 1980' as prefix , albums.name FROM albums WHERE release_date < 1980;
 SELECT 'All albums by Michael Jackson' AS prefix , a.* FROM albums a WHERE artist = 'Michael Jackson';
+
+DESCRIBE albums;
+SELECT * FROM albums;
+
+UPDATE albums SET sales = sales * 10 WHERE sales;
