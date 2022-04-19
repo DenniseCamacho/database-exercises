32c688ee102f85bc955cc81231cea8360b079cc7
New Author Name <dennise.e.camacho@gmail.com>
2022-04-19 11:29:36 -0500

    instruction review/ notes

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,7 +1,9 @@
 USE codeup_test_db;
 
 DELETE a.* FROM albums a WHERE artist = 'One Singer';
+#select * from albums where artist like '%One Singer%';
+# select 'words' as caption, albums.* from albums where artist = 'One Singer';
+#DELETE FROM albums a WHERE artist = 'One Singer';
 DELETE a.* FROM albums a WHERE release_date > 1991;
 DELETE a.* FROM albums a WHERE genre = 'disco';
-
 # Convert the SELECT statements to DELETE.
