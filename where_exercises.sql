a33da84852902be3ec03637086b5327dd4e9825c
New Author Name <dennise.e.camacho@gmail.com>
2022-04-19 15:20:40 -0500

    Update query for Irena, Vidya, or Maya to use OR instead of IN — 709 rows.

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,7 +10,15 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 #     — 7,330 rows.
 SELECT last_name FROM employees
 WHERE last_name LIKE 'E%';
-# Find all employees with a 'q' in their last name — 1,873 rows.
 
+# Find all employees with a 'q' in their last name — 1,873 rows.
 SELECT last_name FROM employees
 WHERE last_name LIKE '%q%';
+
+# Update your query for 'Irena', 'Vidya',
+# or 'Maya' to use OR instead of IN — 709 rows.
+SELECT *
+FROM employees
+WHERE first_name LIKE 'Irena'
+OR first_name LIKE 'Vidya'
+OR first_name LIKE 'Maya';
