8316bc0ab0ab57002ee326dd62f143f3972a4578
New Author Name <dennise.e.camacho@gmail.com>
2022-04-19 10:57:14 -0500

    Convert the SELECT statements to DELETE.

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,7 +1,7 @@
 USE codeup_test_db;
 
-SELECT 'will be deleted' AS prefix , a.* FROM albums a WHERE artist = 'One Singer';
-SELECT 'will be deleted' AS prefix , a.* FROM albums a WHERE release_date > 1991;
-SELECT 'will be deleted' AS prefix , a.* FROM albums a WHERE genre = 'disco';
+DELETE a.* FROM albums a WHERE artist = 'One Singer';
+DELETE a.* FROM albums a WHERE release_date > 1991;
+DELETE a.* FROM albums a WHERE genre = 'disco';
 
-#Make sure to put appropriate captions before each SELECT.
+# Convert the SELECT statements to DELETE.
