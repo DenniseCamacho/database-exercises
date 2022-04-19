0b7b0c535556bfee0b3d086f98395b8d55c5c27e
New Author Name <dennise.e.camacho@gmail.com>
2022-04-19 10:51:37 -0500

    SELECT statements for given statements

---
+++ delete_exercises.sql
@@ -0,0 +1,9 @@
+USE codeup_test_db;
+
+SELECT * FROM albums WHERE release_date > 1991;
+SELECT * FROM albums WHERE genre = 'disco';
+SELECT * FROM albums WHERE artist = 'One Singer';
+# Write SELECT statements for:
+# Albums released after 1991
+# Albums with the genre 'disco'
+#     Albums by 'Whitney Houston' (...or maybe an artist of your choice)
