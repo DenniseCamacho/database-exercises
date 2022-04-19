d24e60f4350f219924c32219d99935c34a82d86a
New Author Name <dennise.e.camacho@gmail.com>
2022-04-19 15:03:18 -0500

    Find all employees whose last name starts with E — 7,330 rows.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,6 +1,12 @@
 USE employees;
-SELECT *
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 # Find all employees with first names 'Irena', 'Vidya',
 # or 'Maya' — 709 rows (Hint: Use IN).
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+#     Find all employees whose
+#     last name starts with 'E'
+#     — 7,330 rows.
+SELECT last_name FROM employees
+WHERE last_name LIKE 'E%';
