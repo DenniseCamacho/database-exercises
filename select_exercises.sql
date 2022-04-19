5590018dfef67da67ad1d6a015082e8042d91e49
New Author Name <dennise.e.camacho@gmail.com>
2022-04-19 11:05:53 -0500

    instruction review/ notes

--- select_exercises.sql
+++ select_exercises.sql
@@ -8,6 +8,17 @@ SELECT * FROM albums WHERE artist = 'Michael Jackson';
 
 SELECT 'The name of all albums by Pink Floyd.' as prefix , a.* FROM albums a WHERE artist = 'Pink Floyd';
 SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' as prefix , a.release_date FROM albums a WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
-SELECT 'The genre for Nevermind' as prefix , a.genre FROM albums a WHERE name = 'Nevermind';
+SELECT 'The genre for Nevermind' as prefix , a.genre
+FROM albums a
+WHERE name = 'Nevermind';
 SELECT 'Which albums were released in the 1990s' as prefix , a.* FROM albums a WHERE release_date = 1990;
 SELECT 'Which albums had less than 20 million certified sales' as prefix , albums.* FROM albums WHERE sales < 20000000;
+
+#SELECT 'words here' AS caption, name
+# FROM albums a
+# WHERE sales < 20;
+#select, from, where
+# SELECT 'rock albums' as caption, a.*
+# FROM albums a
+# where genre like '%Rock%' % is any before any after.. a 'wildcard'
+#makes it not as strict...
