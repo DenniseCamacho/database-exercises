6f5211256ce749195332a4d0479a16269195d2cd
New Author Name <dennise.e.camacho@gmail.com>
2022-04-20 11:03:47 -0500

    complete functions sql exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -35,6 +35,16 @@ ORDER BY birth_date, hire_date DESC;
 
 
 #6. For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
+SELECT CONCAT('Days in company: ', DATEDIFF(NOW(), hire_date))
+    AS `Days at company`,
+    CONCAT(first_name, ' ',last_name)
+        AS `Name`
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
+
 
 SELECT *
 FROM employees
