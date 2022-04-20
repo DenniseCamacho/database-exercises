7806df5e35330955eaf98f37daf506c544df82e8
New Author Name <dennise.e.camacho@gmail.com>
2022-04-20 09:55:00 -0500

    lecture notes on mysql notes

---
+++ functions_lecture.sql
@@ -0,0 +1,24 @@
+USE codeup_test_db;
+SELECT UPPER(CONCAT('Hello', TRIM(' world   ')));
+
+#yr1999-05month-01day
+SELECT CURTIME();
+SELECT UNIX_TIMESTAMP('2022-01-24'); #seconds since
+SELECT * FROM albums WHERE release_date < NOW();
+
+#Aggregate functions.
+SELECT COUNT(*) FROM albums WHERE sales > 24000000;
+
+SELECT COUNT(DISTINCT genre) FROM albums;
+
+SELECT MAX(sales) FROM albums;
+SELECT MIN(sales) FROM albums;
+
+SELECT MIN(sales)
+FROM albums
+WHERE genre = '%Pop%';
+
+SELECT SUM(sales) FROM albums;
+SELECT ROUND(AVG(sales))FROM albums;
+
+SELECT RAND();
