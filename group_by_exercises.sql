a9451f44e70de516a6609f087bc512fd0e46189b
New Author Name <dennise.e.camacho@gmail.com>
2022-04-20 13:56:35 -0500

    group by exercises mysql complete and review

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -24,8 +24,45 @@ WHERE last_name LIKE 'E%E'
 GROUP BY last_name, first_name
 ORDER BY last_name;
 
+#ans:
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%E'
+GROUP BY first_name, last_name;
+
 # Find the unique last names with a 'q' but not 'qu'. Your results should be:
 SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE '%q%'
 AND last_name NOT LIKE '%qu%';
+#ans:
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+# Add a COUNT() to your results and use ORDER BY
+# to make it easier to find employees
+# whose unusual name is shared with others.
+SELECT last_name, COUNT(*)
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY last_name;
+#ans:
+SELECT COUNT(*), last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+# Update your query for 'Irena', 'Vidya', or 'Maya'.
+# Use count(*) and GROUP BY to find the number of employees
+# for each gender with those names. Your results should be:
+
+SELECT COUNT(*), gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
