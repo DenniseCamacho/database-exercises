d9aba359d74e07d03232dcc75e3474b481b9537b
New Author Name <dennise.e.camacho@gmail.com>
2022-04-20 12:03:26 -0500

    Update the query to find just the unique last names that start and end with E using GROUP BY.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,4 +1,16 @@
 USE employees;
 
+
 SELECT DISTINCT title
 FROM titles;
+
+# Find your query for employees whose last names
+# start and end with 'E'. Update the query to find
+# just the unique last names that start and end with 'E'
+# using GROUP BY. The results should be:
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%E'
+GROUP BY last_name
+ORDER BY last_name;
