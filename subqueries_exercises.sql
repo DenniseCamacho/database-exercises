d2c1a8fe89fe71f8e731f02fd0fad716fb8fb91f
New Author Name <dennise.e.camacho@gmail.com>
2022-04-22 10:46:53 -0500

    Problem 1 subqueries

---
+++ subqueries_exercises.sql
@@ -0,0 +1,26 @@
+USE employees;
+
+# Find all the employees with the same hire date
+# as employee 101010 using a subquery.
+# 69 Rows
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'Employee',
+       hire_date AS 'Hire Date'
+FROM employees
+WHERE emp_no =
+    (
+    SELECT emp_no
+    WHERE hire_date = '1990-10-22'
+    );
+
+# Find all the titles held by all employees with the first name Aamod.
+# 314 total titles, 6 unique titles
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'Employees Same Name',
+       title AS 'Titles'
+FROM employees, titles
+WHERE emp_no =
+      (
+          SELECT title
+          WHERE first_name = 'Aamod'
+      );
