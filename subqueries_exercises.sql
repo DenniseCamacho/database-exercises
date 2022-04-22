ca247a1cf257da9de441f935fb36195146f62b86
New Author Name <dennise.e.camacho@gmail.com>
2022-04-22 10:54:00 -0500

    Problem 2 subqueries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -13,14 +13,26 @@ WHERE emp_no =
     WHERE hire_date = '1990-10-22'
     );
 
+
 # Find all the titles held by all employees with the first name Aamod.
 # 314 total titles, 6 unique titles
 
-SELECT CONCAT(first_name, ' ', last_name) AS 'Employees Same Name',
-       title AS 'Titles'
-FROM employees, titles
-WHERE emp_no =
-      (
-          SELECT title
-          WHERE first_name = 'Aamod'
-      );
+SELECT title, COUNT(title)
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    )
+GROUP BY title;
+
+# SELECT CONCAT(first_name, ' ', last_name) AS 'Employees Same Name',
+#        title AS 'Titles'
+# FROM employees, titles
+# WHERE emp_no =
+#       (
+#           SELECT title
+#           WHERE first_name = 'Aamod'
+#       );
+
+# Find all the current department managers that are female.
